c5afd6a6e3baecdcf6bc4275c2b5f05be703944e
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-05-28 17:32:43 +0200

    Update ejercicio1.sh

--- 1º DAM/SISI/BASH/examen/ejercicio1.sh
+++ 1º DAM/SISI/BASH/examen/ejercicio1.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
 
+#funcion del menu
+function menu {
+echo "1) Mostrar hora y fecha"
+echo "2) Mostrar fichero"
+echo "3) Salir"
+}
+op=0
+
+while [ "$op" -ne "3" ]
+do
+ menu
+ echo "Que opción quieres"
+ read op
+ case "$op" in
+	"1")
+	date
+	;;
+	"2")
+	pwd
+	;;
+	"3")
+	exit;;
+	* )
+	echo "Esa opción no está disponible"
+esac
+done
