5ab9dad84f5766db1fe0c7fb0287425f19235822
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-05-28 17:33:01 +0200

    Update ejercicio2.sh

--- 1º DAM/SISI/BASH/examen/ejercicio2.sh
+++ 1º DAM/SISI/BASH/examen/ejercicio2.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+for i in 1 2 3 4 5
+do
+echo "Digame un número:"
+read num
+	if [ $((num%2)) -eq "0" ]; then
+		echo "ES PAR"
+	elif [ $((num%2)) -ne "0"  ]; then
+		echo "ES IMPAR"
+	fi
+done
