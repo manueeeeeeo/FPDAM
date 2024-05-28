691977c0957f116858128c05b38efdf0fda0777d
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-05-28 17:33:18 +0200

    Update ejercicio3.sh

--- 1º DAM/SISI/BASH/examen/ejercicio3.sh
+++ 1º DAM/SISI/BASH/examen/ejercicio3.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+#Para crear el documento
+if [ -e "ejemplo.txt" ]; then
+	echo "Ya esta creado"
+else
+	touch "ejemplo.txt"
+	echo "Hola, Mundo!" >> "ejemplo.txt"
+	echo "Esto es un fichero de ejemplo" >> "ejemplo.txt"
+	echo "¡Adiós!" >> "ejemplo.txt"
+fi
+
+#Para cambiar el fchero por ejemplo
+
+sed -i "s/fichero/archivo/" ejemplo.txt
