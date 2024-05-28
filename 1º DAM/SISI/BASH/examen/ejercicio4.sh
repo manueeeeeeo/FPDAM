159c31cef4beaffbbfa720d8923528c5b4f93b62
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-05-28 17:33:47 +0200

    Update ejercicio4.sh

--- 1º DAM/SISI/BASH/examen/ejercicio4.sh
+++ 1º DAM/SISI/BASH/examen/ejercicio4.sh
@@ -1,1 +1,55 @@
+#!/bin/bash
 
+function menu {
+echo "1) Sumar"
+echo "2) Restar"
+echo "3) Multiplicar"
+echo "4) Dividir"
+echo ""
+}
+
+echo "Introduzca el primer número: "
+read num1
+echo "Introduzca el seundo número: "
+read num2
+
+function sumar {
+var=$(($num1+$num2))
+echo "La suma es: $var"
+}
+
+function resta {
+var=$(($num1-$num2))
+echo "La resta es: $var"
+}
+
+function mult {
+var=$(($num1*$num2))
+echo "La mutiplicacion es: $var"
+}
+
+function div {
+var=$(($num1/$num2))
+echo "La divison es: $var"
+}
+
+menu
+echo "Que opción eliges:"
+read op
+
+case "$op" in
+	"1" )
+	 sumar
+	;;
+	"2" )
+	resta
+	;;
+	"3" )
+	mult
+	;;
+	"4" )
+	div
+	;;
+	* )
+	;;
+esac
