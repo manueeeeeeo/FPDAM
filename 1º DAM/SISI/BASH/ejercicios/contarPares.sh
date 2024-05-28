f2c8fac882e5904df1ef68e0c82153cfa6c66060
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-05-28 17:34:23 +0200

    Update contarPares.sh

--- 1º DAM/SISI/BASH/ejercicios/contarPares.sh
+++ 1º DAM/SISI/BASH/ejercicios/contarPares.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+pares=""
+impares=""
+
+echo "Hasta que númeo quieres llegar:"
+read resp
+
+for((i=0; i<=$resp; i++))
+do
+        if [ $((i%2)) -eq 0 ]; then
+                pares="$pares $i"
+        elif [ $((i%2)) -eq 1 ]; then
+                impares="$impares $i"
+        fi
+done
+echo "Números pares: "
+echo "$pares"
+echo "Números impares: "
+echo "$impares"
