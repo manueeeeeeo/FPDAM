2eaf7f7b1033f8f13ac36f1bc1a4354ac8b5adad
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-05 18:16:33 +0200

    Update SumarMatrices.java

--- 1º DAM/PROGRAMACION/UNIDADES/MATRICES/SumarMatrices.java
+++ 1º DAM/PROGRAMACION/UNIDADES/MATRICES/SumarMatrices.java
@@ -1,1 +1,66 @@
+package matrices;
 
+import java.util.Scanner;
+
+public class SumarMatrices {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		// Variables
+		int[][] matriz1 = new int[3][4];
+		int[][] matriz2 = new int[3][4];
+		int[][] suma = new int[3][4];
+
+		// Rellenar matrices
+		rellenarMatriz(matriz1);
+		rellenarMatriz(matriz2);
+		sumarMatrices(matriz1, matriz2, suma);
+
+		// Mostrar matrices
+		imprimirMatrices(matriz1, matriz2, suma);
+		teclado.close();
+	}
+
+  // Método para imprimir todas las matrices
+	public static void imprimirMatrices(int[][] matriz1, int[][] matriz2, int[][] suma) {
+		for (int i = 0; i < matriz1.length; i++) {
+			System.out.println();
+			for (int j = 0; j < matriz1[0].length; j++) {
+				System.out.print(matriz1[i][j] + "\t");
+			}
+			if(i==1) {
+				System.out.print(" + ");
+			}
+			System.out.print("\t");
+			for(int j = 0;j<matriz2[0].length;j++) {
+				System.out.print(matriz2[i][j] + "\t");
+			}
+			if(i==1) {
+				System.out.print(" = ");
+			}
+			System.out.print("\t");
+			for(int j = 0;j<suma[0].length;j++) {
+				System.out.print(suma[i][j] + "\t");
+			}
+		}
+	}
+
+  // Método para sumar las matrices
+	public static void sumarMatrices(int[][] matriz1, int[][] matriz2, int[][] suma) {
+		for (int i = 0; i < suma.length; i++) {
+			for (int j = 0; j < suma[0].length; j++) {
+				suma[i][j] = matriz1[i][j] + matriz2[i][j];
+			}
+		}
+	}
+
+  // Método para rellenar las matrices
+	public static void rellenarMatriz(int[][] matriz) {
+		int numero = 0;
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[0].length; j++) {
+				numero = (int) (Math.random() * 30 + 1);
+				matriz[i][j] = numero;
+			}
+		}
+	}
+}
