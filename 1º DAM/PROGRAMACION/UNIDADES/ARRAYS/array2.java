a552f96e067ba1c4259028358db5b992e69dd643
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-05 18:07:53 +0200

    Update array2.java

--- 1º DAM/PROGRAMACION/UNIDADES/ARRAYS/array2.java
+++ 1º DAM/PROGRAMACION/UNIDADES/ARRAYS/array2.java
@@ -1,1 +1,41 @@
+package arrays;
 
+import java.util.Scanner;
+
+public class Array2 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		//Declaro el array
+		int [] num = new int[10];
+		int contadorpos = 0;
+		int contadorneg = 0;
+		int contador0 = 0;
+		
+		//Para que el usuario rellene los datos
+		for(int i=0;i<num.length;i++) {
+			System.out.print("Digame un número: ");
+			num[i] = teclado.nextInt();
+		}
+		
+		//Para obtener los numeros y saber cuantos pos,neg, y 0 hay
+		for(int j=0;j<num.length;j++) {
+			if(num[j]>0) {
+				contadorpos++;
+			}else if(num[j]<0) {
+				contadorneg++;
+			}else {
+				contador0++;
+			}
+		}
+		if(contadorpos>0) {
+			System.out.println("El número de valores positivos es: "+contadorpos);
+		}
+		if(contadorneg>0) {
+			System.out.println("El número de valores positivos es: "+contadorneg);
+		}
+		if(contador0>0) {
+			System.out.println("El número de valores positivos es: "+contador0);
+		}
+		teclado.close();
+	}
+}
