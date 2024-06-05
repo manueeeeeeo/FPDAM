5862ce37c6bb7853dff5c8dc247b65773f1bf48b
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-05 18:11:52 +0200

    Update SaberSiEsPrimo.java

--- 1º DAM/PROGRAMACION/UNIDADES/METODOS/SaberSiEsPrimo.java
+++ 1º DAM/PROGRAMACION/UNIDADES/METODOS/SaberSiEsPrimo.java
@@ -1,1 +1,36 @@
+package ejercicios;
 
+import java.util.Scanner;
+
+public class SaberSiEsPrimo {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		// Declaro las variables
+		int num = 0;
+		int primo = 0;
+		int contador = 0;
+
+		System.out.print("Digame el valor del número: ");
+		num = teclado.nextInt();
+
+		primo = esprimo(num, contador, primo);
+		if (primo == 1) {
+			System.out.println("Es primo");
+		} else {
+			System.out.println("No es primo");
+		}
+		teclado.close();
+	}
+
+	private static int esprimo(int num, int cont, int primo) {
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				cont++;
+			}
+		}
+		if (cont == 2) {
+			primo++;
+		}
+		return (primo);
+	}
+}
