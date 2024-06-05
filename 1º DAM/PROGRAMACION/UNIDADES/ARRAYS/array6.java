962cdce51928ab8f5a7d3fdc25381f41153723e3
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-05 18:09:49 +0200

    Update array6.java

--- 1º DAM/PROGRAMACION/UNIDADES/ARRAYS/array6.java
+++ 1º DAM/PROGRAMACION/UNIDADES/ARRAYS/array6.java
@@ -1,1 +1,23 @@
+package arrays;
 
+import java.util.Scanner;
+
+public class Array6 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		String [] nombre = new String [5];
+		int [] edad = new int [5];
+		
+		for(int i = 0;i<nombre.length;i++) {
+			System.out.print("Digame el nombre del alumno: ");
+			nombre[i] = teclado.next();
+			System.out.print("Digame la edad de "+nombre[i]+": ");
+			edad [i] = teclado.nextInt();
+		}
+		System.out.println();
+		for(int j = 0;j<nombre.length;j++) {
+			System.out.println("El alumno "+nombre[j]+", tiene "+edad[j]+" años");
+		}
+		teclado.close();
+	}
+}
