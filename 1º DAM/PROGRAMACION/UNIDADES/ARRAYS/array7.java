d7d257511386096fa0fad41c1cf69e5f1fe58eff
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-05 18:10:14 +0200

    Update array7.java

--- 1º DAM/PROGRAMACION/UNIDADES/ARRAYS/array7.java
+++ 1º DAM/PROGRAMACION/UNIDADES/ARRAYS/array7.java
@@ -1,1 +1,29 @@
+package arrays;
 
+import java.util.Scanner;
+
+public class Array7 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		// Declaro los arrays
+		String[] conductor = new String[4];
+		int[] km = new int[4];
+		int kmtotal = 0;
+
+		for (int i = 0; i < conductor.length; i++) {
+			System.out.print("Digame el nombre del conductor: ");
+			conductor[i] = teclado.next();
+			System.out.print("Digame el Nº de Km que ha hecho este més " + conductor[i] + ": ");
+			km[i] = teclado.nextInt();
+			kmtotal += km[i];
+		}
+		System.out.println();
+
+		for (int j = 0; j < conductor.length; j++) {
+			System.out.println("El conductor "+conductor[j]+" hizo "+km[j]+" Km este més");
+		}
+		System.out.println();
+		System.out.println("El número total de Km este més fued de: "+kmtotal);
+		teclado.close();
+	}
+}
