ce753843cf22668ef41102afc220ed99a535991c
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-06 12:41:30 +0200

    Update Dado.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO SIMPLE/Dado.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO SIMPLE/Dado.java
@@ -1,1 +1,30 @@
+package poo;
+public class Dado {
+	//Variables
+	private int nCara;
 
+	//Constructor por defecto
+	public Dado() {
+		this.nCara = 0;
+	}
+	
+	//Getters y setters
+	public int getnCara() {
+		return nCara;
+	}
+
+	public void setnCara(int nCara) {
+		this.nCara = nCara;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return " ---"+"\n| "+nCara+" |\n"+" ---";
+	}
+	
+	//Lanzar el dado
+	public  void lanzarDado() {
+		this.nCara = (int)(Math.random()*6+1);
+	}
+}
