59e232bab150f8aea550c2b6de739014d4f627b1
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-07 12:35:17 +0200

    Update Moto.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/dueñosveh/Moto.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/dueñosveh/Moto.java
@@ -1,1 +1,39 @@
+package dueñosVeh;
 
+public class Moto extends Vehiculo {
+	// Atributos propios
+	private double cilindrada;
+
+	// Constructor predefinido
+	public Moto() {
+		super();
+		cilindrada = 0;
+	}
+
+	// Constructor con todos los parametros
+	public Moto(String matricula, String marca, String modelo, double cilindrada) {
+		super(matricula, marca, modelo);
+		this.cilindrada = cilindrada;
+	}
+
+	// Getters y Setters
+	public double getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(double cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Moto [cilindrada=" + cilindrada + ", matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo
+				+ "]";
+	}
+
+	// Para saber el tipo de vehiculo que es
+	public String getTipo() {
+		return "Moto";
+	}
+}
