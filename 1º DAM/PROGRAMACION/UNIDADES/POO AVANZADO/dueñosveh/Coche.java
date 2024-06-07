616a9114af72091a7417779e38238f4082936938
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-07 12:34:56 +0200

    Update Coche.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/dueñosveh/Coche.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/dueñosveh/Coche.java
@@ -1,1 +1,50 @@
+package dueñosVeh;
 
+public class Coche extends Vehiculo {
+	//Atributos propios
+	private String color;
+	private int nPuertas;
+	
+	//Constructor predefinido
+	public Coche() {
+		super();
+		this.color = "";
+		this.nPuertas = 0;
+	}
+	
+	//Constructor con todos los parametros
+	public Coche(String matricula, String marca, String modelo, String color, int nPuertas) {
+		super(matricula, marca, modelo);
+		this.color = color;
+		this.nPuertas = nPuertas;
+	}
+	
+	//Getters y Setters
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getnPuertas() {
+		return nPuertas;
+	}
+
+	public void setnPuertas(int nPuertas) {
+		this.nPuertas = nPuertas;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Coche [color=" + color + ", nPuertas=" + nPuertas + ", matricula=" + matricula + ", marca=" + marca
+				+ ", modelo=" + modelo + "]";
+	}
+	
+	//Para saber el tipo de vehiculo que es
+	public String getTipo() {
+		return "Coche";
+	}
+}
