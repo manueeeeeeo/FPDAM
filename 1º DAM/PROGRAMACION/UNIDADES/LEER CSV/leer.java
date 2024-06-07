f17f79b79eaa6d9588f85c867fb7e780aef13ca7
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-07 15:53:57 +0200

    Update leer.java

--- 1º DAM/PROGRAMACION/UNIDADES/LEER CSV/leer.java
+++ 1º DAM/PROGRAMACION/UNIDADES/LEER CSV/leer.java
@@ -1,1 +1,16 @@
+//En este caso leeremos peliculas de un fichero csv, es igual que el paquete de cine
+//Una vez coges la estructura resulta facil
 
+public ArrayList cargarPelis(String fichero) throws FileNotFoundException {
+		Scanner entrada = new Scanner(new File(fichero));
+		ArrayList<Pelicula> peli = new ArrayList<Pelicula>();
+		String linea[];
+		String cadena;
+		entrada.nextLine(); //línea de cabezera
+		while (entrada.hasNext()) {
+			cadena = entrada.nextLine();
+			linea = cadena.split(";");
+			peli.add(new Pelicula(Integer.parseInt(linea[0]), linea[1], linea[2], linea[3]));
+		}
+		return peli;
+	}
