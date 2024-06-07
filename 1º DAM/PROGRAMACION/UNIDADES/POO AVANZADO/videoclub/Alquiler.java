dccf16a62304b3a5509052d8f83ad40e7d92ac64
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-07 12:31:07 +0200

    Update Alquiler.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/videoclub/Alquiler.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/videoclub/Alquiler.java
@@ -1,1 +1,45 @@
+package videoclub;
 
+public class Alquiler {
+	private int tiempo;
+	private DVD dvd;
+	
+	//Constructor predefinido
+	public Alquiler() {
+		this.tiempo=0;
+		this.dvd=new DVD();
+	}
+	
+	//Constructor profesinal
+	public Alquiler(int tiempo, DVD dvd) {
+		this.tiempo = tiempo;
+		this.dvd = dvd;
+	}
+
+	//Getters and Setters
+	public int getTiempo() {
+		return tiempo;
+	}
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+	public DVD getDvd() {
+		return dvd;
+	}
+	public void setDvd(DVD dvd) {
+		this.dvd = dvd;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Alquiler [tiempo=" + tiempo + ", dvd=" + dvd + "]";
+	}
+
+  // Prueba unitaria
+	public static void main(String[] args) {
+		DVD mulan = new DVD("Mulan", DVD.INFANTIL);
+		Alquiler a = new Alquiler(3, mulan);
+		System.out.println(a);
+	}
+}
