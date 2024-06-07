0d910aefae67a3eb3cf2138d34a56ce2d43ce8bf
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-07 12:34:35 +0200

    Update Vehiculo.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/dueñosveh/Vehiculo.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/dueñosveh/Vehiculo.java
@@ -1,1 +1,59 @@
+package dueñosVeh;
 
+public abstract class Vehiculo {
+	// Atributos
+	protected String matricula;
+	protected String marca;
+	protected String modelo;
+
+	// Constructor predefinido
+	public Vehiculo() {
+		this.matricula = "";
+		this.marca = "";
+		this.modelo = "";
+	}
+
+	//Cosntructor con todos los parametros
+	public Vehiculo(String matricula, String marca, String modelo) {
+		super();
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+
+	//Getters y Setters
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + "]";
+	}
+	
+	//Método para obtener el tipo
+	public String getTipo() {
+		return "";
+	}
+}
