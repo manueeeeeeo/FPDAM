bf96eb6ff4fa57a167d1c66731b8c0a8eae3386f
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-07 15:48:38 +0200

    Update Persona.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/hundeLaFlota/Persona.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/hundeLaFlota/Persona.java
@@ -1,1 +1,71 @@
+package examenBarcos;
 
+import java.util.Arrays;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+	private String nick;
+	private Boleto b1;
+	
+	//Constructor por defecto
+	public Persona() {
+		nombre = "";
+		edad = 0;
+		nick = "";
+		b1 = new Boleto(6);
+		b1.rellenarBoleto(b1.getB(), 6);
+	}
+	
+	//Constructor 1
+	public Persona(String nombre, int edad) {
+		this.nombre = "";
+		this.edad = 0;
+		if(nombre.length()>3) {
+			this.nick = "Jugador_"+nombre.substring(0, 3)+edad;
+		}else {
+			this.nick = "Jugador_"+nombre+edad;
+		}
+		b1 = new Boleto(6);
+		b1.rellenarBoleto(b1.getB(), 6);
+	}
+
+	//Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public Boleto getB1() {
+		return b1;
+	}
+
+	public void setB1(Boleto b1) {
+		this.b1 = b1;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "El jugador "+this.nick+" tiene el boleto "+Arrays.toString(this.b1.getB());
+	}
+}
