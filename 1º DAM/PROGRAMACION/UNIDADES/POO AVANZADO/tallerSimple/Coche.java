bf9223d754d987173011873dd639ad4af8ad8149
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-09 13:14:10 +0200

    Update Coche.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/tallerSimple/Coche.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/tallerSimple/Coche.java
@@ -1,1 +1,42 @@
+package taller;
 
+public class Coche extends Vehiculo {
+	private String color;
+	private int nPuertas;
+	private double km;
+	public Coche(String matricula, String marca, String modelo, String color, int nPuertas, double km) {
+		super(matricula, marca, modelo);
+		this.color = color;
+		this.nPuertas = nPuertas;
+		this.km = km;
+	}
+	public Coche() {
+		super();
+		this.color ="";
+		this.nPuertas = 0;
+		this.km = 0.0;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getnPuertas() {
+		return nPuertas;
+	}
+	public void setnPuertas(int nPuertas) {
+		this.nPuertas = nPuertas;
+	}
+	public double getKm() {
+		return km;
+	}
+	public void setKm(double km) {
+		this.km = km;
+	}
+	@Override
+	public String toString() {
+		return "Coche [color=" + color + ", nPuertas=" + nPuertas + ", km=" + km + ", matricula=" + matricula
+				+ ", marca=" + marca + ", modelo=" + modelo + "]";
+	}
+}
