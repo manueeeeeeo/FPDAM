f65d6f4533da664e4abb76557891741e4ab8d96c
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-09 13:13:30 +0200

    Update Vehiculo.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/tallerSimple/Vehiculo.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/tallerSimple/Vehiculo.java
@@ -1,1 +1,49 @@
+package taller;
 
+public class Vehiculo {
+	protected String matricula;
+	protected String marca;
+	protected String modelo;
+	public Vehiculo(String matricula, String marca, String modelo) {
+		super();
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	
+	public Vehiculo() {
+		this.matricula = "";
+		this.marca = "";
+		this.modelo = "";
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + "]";
+	}
+
+}
