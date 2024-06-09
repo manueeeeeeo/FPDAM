767762d93e2ddd534b6d91c43e3bd5d9c4ecdd51
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-09 13:13:52 +0200

    Update Moto.java

--- 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/tallerSimple/Moto.java
+++ 1º DAM/PROGRAMACION/UNIDADES/POO AVANZADO/tallerSimple/Moto.java
@@ -1,1 +1,29 @@
+package taller;
 
+public class Moto extends Vehiculo {
+	private double cilindrada;
+
+	public Moto(String matricula, String marca, String modelo, double cilindrada) {
+		super(matricula, marca, modelo);
+		this.cilindrada = cilindrada;
+	}
+
+	public Moto() {
+		super();
+		this.cilindrada=0;
+	}
+
+	public double getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(double cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	@Override
+	public String toString() {
+		return "Moto [cilindrada=" + cilindrada + ", matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo
+				+ "]";
+	}
+}
