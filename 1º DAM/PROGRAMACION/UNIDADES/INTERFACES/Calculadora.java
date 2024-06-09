935463d805cc92f54907c016f7c1cb8ad06964e8
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-09 13:22:11 +0200

    Update Calculadora.java

--- 1º DAM/PROGRAMACION/UNIDADES/INTERFACES/Calculadora.java
+++ 1º DAM/PROGRAMACION/UNIDADES/INTERFACES/Calculadora.java
@@ -1,1 +1,28 @@
+package interfaces;
 
+public class Calculadora implements Matematicas{
+
+	@Override
+	public int suma(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public float suma(float a, float b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public double suma(double a, double b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public int multiplicacion(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+}
