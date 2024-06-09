5ca5c27ee800316a2b1244e77e3c4f22de1100d6
manueeeeeeo <146026153+manueeeeeeo@users.noreply.github.com>
2024-06-09 13:22:28 +0200

    Update Matematicas.java

--- 1º DAM/PROGRAMACION/UNIDADES/INTERFACES/Matematicas.java
+++ 1º DAM/PROGRAMACION/UNIDADES/INTERFACES/Matematicas.java
@@ -1,1 +1,8 @@
+package interfaces;
 
+public interface Matematicas {
+	public int suma(int a, int b);
+	public float suma(float a, float b);
+	public double suma (double a, double b);
+	public int multiplicacion(int a, int b);
+}
