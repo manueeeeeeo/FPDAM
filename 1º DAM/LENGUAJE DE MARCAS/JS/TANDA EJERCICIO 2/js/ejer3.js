e957e3929aa909d0d36fbde9e00d9590ccb2872e
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-02 22:58:06 +0200

    Update ejer3.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIO 2/js/ejer3.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIO 2/js/ejer3.js
@@ -1,1 +1,9 @@
+let n1 = prompt("Digame el primer número: ");
+let n2 = prompt("Digame el exponente: ");
+let pot = 1; 
 
+for (let i = 1; i <= n2; i++) {
+    pot = pot * n1; 
+}
+
+alert("La potencia de " + n1 + " es: " + pot);
