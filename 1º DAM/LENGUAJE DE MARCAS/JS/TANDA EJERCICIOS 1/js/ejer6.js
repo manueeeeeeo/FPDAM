b7e43e3f3729b306487c46db7fc11071e4a164fa
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-02 22:51:14 +0200

    Update ejer6.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer6.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer6.js
@@ -1,1 +1,12 @@
+const campo = document.getElementById('letras'); //Obtengo el input
 
+document.addEventListener("DOMContentLoaded", function() {
+    campo.addEventListener("input", borrarNum); //Cada vez que escriba en el input hace la función de borrar números
+    function borrarNum(){
+        let valor =campo.value;
+        let letras2 = valor.split(''); //Voy añadiendo a un array todo lo que voy escribiendo
+        console.log(letras2);
+        let letrasSolo = letras2.filter(caracter => isNaN(caracter)); //Voy filtrando para que no me deje escribir números en otro array
+        campo.value = letrasSolo.join(''); //El valor del input se actualzia con el del array sin números
+    }
+});
