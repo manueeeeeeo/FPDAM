1f8c816a2ca947a67d89f77b8a77742c16c93cbd
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-02 22:49:03 +0200

    Update ejer2..js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer2..js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer2..js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const di = document.getElementById("area");
+    di.addEventListener("click", myfunction); //Al tocar el botón llamamos a mi función
 
+    function myfunction() {
+        alert("Hola, esto es un alert");
+    }
+});
