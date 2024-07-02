187757319345d8d44caa66a3faca41af2e3b9b11
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-02 22:57:33 +0200

    Update ejer2.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIO 2/js/ejer2.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIO 2/js/ejer2.js
@@ -1,1 +1,11 @@
+let n = prompt("Digame un valor: ");
+let fac = 0;
 
+for (let i = 1; i<=n; i++){
+    if (i==1){
+        fac = i;
+    }else{
+        fac = fac*i;
+    }
+}
+alert("5! es igual a: "+fac)
