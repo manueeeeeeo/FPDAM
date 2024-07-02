97804fc91ae3899a68b02776c9ca8ad4293fa268
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-02 22:49:32 +0200

    Update ejer3.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer3.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer3.js
@@ -1,1 +1,15 @@
+const espacio = document.getElementById('efecto');
+const primero = document.getElementById('pasar');
 
+primero.addEventListener("mouseover", cambiarColor); //Si el ratón está encima cambiamos el color
+primero.addEventListener("mouseout", volverColor); //Si sale de encima volvemos al color principal
+
+function cambiarColor () {
+    espacio.style.backgroundColor = 'green';
+    espacio.style.color = 'white';
+}
+
+function volverColor () {
+    espacio.style.backgroundColor = 'white';
+    espacio.style.color = 'black';
+}
