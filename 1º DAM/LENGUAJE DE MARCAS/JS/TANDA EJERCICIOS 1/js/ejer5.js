53fd78a65a8731d9c15914c0a15a60370c019440
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-02 22:50:30 +0200

    Update ejer5.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer5.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer5.js
@@ -1,1 +1,13 @@
+const cuadrado = document.getElementById('cuadrado');
 
+document.addEventListener("DOMContentLoaded", function() {
+    cuadrado.addEventListener("mouseover", ponerRojo);
+    cuadrado.addEventListener("mouseout", ponerNormal);
+    function ponerRojo(){
+        cuadrado.style.background = 'red';
+    }
+    
+    function ponerNormal(){
+        cuadrado.style.background = 'white';
+    }
+});
