9624971e7fcfffc8081331a8e9ca3c651bbcd5e1
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-02 22:50:03 +0200

    Update ejer4.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer4.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 1/js/ejer4.js
@@ -1,1 +1,11 @@
+const boton = document.getElementById('enviar');
+const resul = document.getElementById('total');
 
+boton.addEventListener("click", calculo);
+
+function calculo (){
+    let pre = document.getElementById('precio').value; //Obtengo el valor del precio
+    let cant = document.getElementById('cantidad').value; //Obtengo la cantidad de veces que quiero el producto
+    console.log(parseFloat(pre)*parseFloat(cant));
+    resul.value = parseFloat(pre)*parseFloat(cant) //Lo multiplico y le estblezco ese valor al input del resultado
+}
