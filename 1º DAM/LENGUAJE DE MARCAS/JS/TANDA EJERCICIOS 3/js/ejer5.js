f6ab62f76b5111ed82b552473f5a0d9031cb9aad
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 17:04:27 +0200

    Update ejer5.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer5.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer5.js
@@ -1,1 +1,10 @@
+const piramide = () =>{
+    for (let i=1; i<=30;i++){ // Hacemos un for para ir de 1 al 30
+        for(j=1;j<=i;j++){ // Imprimimos el número de veces el número que es
+            document.write(i+" ");
+        }
+        document.write("<br>");
+    }
+}
 
+piramide();
