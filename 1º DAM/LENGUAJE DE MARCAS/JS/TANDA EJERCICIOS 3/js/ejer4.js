1621d0d0c4bfc97d51f242df47e8c8797248fead
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 17:04:01 +0200

    Update ejer4.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer4.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer4.js
@@ -1,1 +1,16 @@
+const sumar = () =>{
+    while(confirm("Quieres introducir más números?")){ // Mientras que sigamos queriendo meter números hacemos el bucle
+        let num = prompt("Introduce un número");
+        if(num.match("[A-Z a-z]")){ // Comrpobamos que la variable no contenga una letra y lo que pasa
+            alert("Eso no es un número");
+        }else{ // En caso de que sean números los sumamos
+            sum = sum + parseInt(num);
+        }
+    }
+}
 
+let sum = 0; // Inicializamos una variable = 0
+
+sumar();
+
+alert("La suma es: "+sum); // Imprimimos el resultado de la suma
