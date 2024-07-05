12a653028535009c63f7abd36d783994e67e7639
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 17:01:44 +0200

    Update ejer1.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer1.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer1.js
@@ -1,1 +1,11 @@
+const determinarEdad = (edad) =>{
+    if(edad>=18){ // Saber si es mayor de 18 y lo que pasa si es así o si no lo es
+        return(alert("Si que puedes conducir"));
+    }else{
+        return(alert("No puedes conducir"));
+    }
+}
 
+let edad = prompt("Introduce tu edad: "); // Le damos el valor por pantalla a edad
+
+determinarEdad(edad);
