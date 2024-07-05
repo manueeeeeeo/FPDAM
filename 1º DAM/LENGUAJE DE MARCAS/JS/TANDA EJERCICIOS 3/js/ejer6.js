f401b209c1e56c74008209ad67e475025b34ca28
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 17:04:54 +0200

    Update ejer6.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer6.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer6.js
@@ -1,1 +1,21 @@
+const saberMultiplos = () =>{
+    for(let i = 1; i<=500; i++){
+        if(i%4==0){
+            document.write(i+"(Múltiplo de 4)");
+        }else if(i%9==0){
+            document.write(i+"(Múltiplo de 9)");
+        }else{
+            document.write(i);
+        }
+        cont++;
+        if(cont==5){
+            document.write("<br>"+"--------------------------")
+            cont=0;
+        }
+        document.write("<br>");
+    }
+}
 
+let cont = 0;
+
+saberMultiplos();
