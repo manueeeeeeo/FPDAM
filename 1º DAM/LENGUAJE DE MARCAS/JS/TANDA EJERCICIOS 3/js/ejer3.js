bbc5295e53d6a775b368d9a89e732096bf8fd63a
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 17:03:34 +0200

    Update ejer3.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer3.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer3.js
@@ -1,1 +1,12 @@
+const concatenar = () =>{
+    do{
+        let respuesta = prompt("Introduce una cadena");
+        cadena = cadena + "-" + respuesta; // Concatenas las variables para que al final te de el resutado
+    }while(confirm("Quieres introducir otra cadena")); // Que haga el bucle mientras que sigas queriendo meter cadenas
+}
 
+let cadena=prompt("Introduce una cadena"); // Le damos un valor por pantalla a la variable cadena
+
+concatenar();
+
+alert(cadena); // Imprimimos el resultado
