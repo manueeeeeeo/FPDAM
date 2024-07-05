0223bdef93cd50953aa4c89e12aeea52f3daa398
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 17:05:23 +0200

    Update ejer7.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer7.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer7.js
@@ -1,1 +1,39 @@
+const saberMayor = () =>{
+    alert("Apartado 1:");
+    let var1 = valores[3];
+    let var2 = valores[4];
+    
+    if (var1.length > var2.length) {
+        alert("Es mayor: "+var1);
+    } else if (var2.length > var1.length) {
+        alert("Es mayor: "+var2);
+    } else {
+        alert("Tinen la misma longitud");
+    }
+}
 
+const verFalsoVerdadero = () =>{
+    alert("Apartado 2:");
+    let resul1 = valores[0] && valores[2];
+    let resul2 = valores[0] || valores[2];
+    
+    alert("Resultado 1:" + resul1);
+    alert("Resultado 2:" + resul2);
+}
+
+const operacionesBasicas = () =>{
+    alert("Apartado 3:");
+    let num1 = valores[1];
+    let num2 = valores[5];
+    
+    alert("Suma:" + (parseInt(num1) + parseInt(num2)));
+    alert("Resta:" + (parseInt(num1) - parseInt(num2)));
+    alert("Multiplicación:" + (parseInt(num1) * parseInt(num2)));
+    alert("División:" + (parseInt(num1) / parseInt(num2)));
+    alert("Resto:" + (parseInt(num1) % parseInt(num2)));
+}
+
+let valores = [true, 5, false, "hola", "adios", 2];
+saberMayor();
+verFalsoVerdadero();
+operacionesBasicas();
