24f9e29383b7e80ea1ec2897bb28644db1c893d4
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 17:05:49 +0200

    Update ejer8.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer8.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 3/js/ejer8.js
@@ -1,1 +1,14 @@
+const calcularFactorial = () =>{
+    for (let i = 1; i<=n; i++){
+        if (i==1){
+            fac = i;
+        }else{
+            fac = fac*i;
+        }
+    }
+}
 
+let n = prompt("Digame un valor: ");
+let fac = 0;
+calcularFactorial();
+alert("5! es igual a: "+fac)
