06a44860ebc2a924c82414d659ec20e299b1ef41
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:08:46 +0200

    Update ejer7.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer7.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer7.js
@@ -1,1 +1,15 @@
+const ordenar = (num)=>{
+    let numeros = [];
+    cont=0;
+    for(let i = num;i>=1;i--){
+        numeros[cont]=i;
+        cont++;
+    }
+    return numeros;
+}
 
+let num = prompt("Digame un número");
+document.write("El array del "+num+" al 1 es: <br>");
+for(let num1 of ordenar(num)){
+    document.write(num1+" ");
+}
