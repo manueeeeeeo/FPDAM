c0f18b86b89e40a8dc2d83bbf074cec1007f8754
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:06:24 +0200

    Update ejer5.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer5.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer5.js
@@ -1,1 +1,17 @@
+const numeroCuadrado = (n) =>{
+    let cuadrado = 0;
+    cuadrado = Math.pow(n,2);
+    return cuadrado;
+}
 
+const numeroCubo = (n) =>{
+    let cubo = 0;
+    cubo = Math.pow(n,3);
+    return cubo;
+}
+
+let num = prompt("Digame un número: ");
+let n = numeroCuadrado(num);
+let c = numeroCubo(num);
+document.write("El cuadrado de "+num+" es: "+n);
+document.write("<br>El cubo de "+num+" es: "+c);
