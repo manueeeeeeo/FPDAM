3afd21cfbc84ec6fc56516fb44ef0fbba7ad32c6
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:05:55 +0200

    Update ejer4.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer4.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer4.js
@@ -1,1 +1,11 @@
+const findIndex = (num, bus)=>{
+    for(let i = 0;i<num.length;i++){
+        if(num[i]===bus){
+            document.write("El elemento "+bus+" ha sido encontrado en la posición "+i);
+        }
+    }
+}
 
+let arr = new Array(3,6,46,24,7,8);
+let num = prompt("Digame el número que quiere buscar");
+findIndex(arr, num);
