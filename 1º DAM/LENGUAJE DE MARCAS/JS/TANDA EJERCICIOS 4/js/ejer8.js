1428965f0cd858ed6d9cd7675e152dfce4e72d42
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:09:15 +0200

    Update ejer8.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer8.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer8.js
@@ -1,1 +1,23 @@
+const quitarRepes =(array) =>{
+    let auxae = [];
+    let cont = 0;
+    for(let i = 0;i<array.length;i++){
+        for (let j=0;j<array.length;j++){
+            if(array[i]==array[j]){
+                cont++;
+            }
+        }
+        if(cont===1){
+            auxae[i]=array[i];
+        }
+        cont = 0;
+    }
+    return auxae;
+}
 
+let arr = new Array(3,4,5,6,3,4,9,1,2);
+let arrayNuevo = quitarRepes(arr);
+document.write("<h1>El array con los repes quitados queda: </h1>");
+arrayNuevo.forEach(element => {
+    document.write(element+" ");
+});
