c8f2a5292c811f4fcb9874c9737c1ff5b9f270e6
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:04:54 +0200

    Update ejer2.js

--- 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer2.js
+++ 1º DAM/LENGUAJE DE MARCAS/JS/TANDA EJERCICIOS 4/js/ejer2.js
@@ -1,1 +1,17 @@
+const cambiarPos = (pos1, pos2)=>{
+    let aux = arr[pos1];
+    arr[pos1] = arr[pos2];
+    arr[pos2] = aux;
+    //Sustituimos al aux por uno, le cambiamos por otro y luego le damos
+    //ese valor a la pos dos
+}
 
+let arr = new Array (1,2,3);
+let pos1 = prompt("Que indice quieres cambiar?");
+let pos2 = prompt("Por que posición?");
+
+cambiarPos(pos1, pos2);
+
+for(let i = 0;i<arr.length;i++){
+    document.write(arr[i]+" ");
+}
